03bfae481f756fad5c67c70072654da9196f47b1
Mixael <Mixaeljcb@gmail.com>
2019-08-12 13:12:11 -0500

    Added browser self-refresh on css and html changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nestvars = require('postcss-nested');
+const cssImport = require('postcss-import');
+const browsrSync = require('browser-sync').create();
 
 gulp.task('default', function(done){
     console.log("Congrats in the first gulp task ");
@@ -11,20 +13,32 @@ gulp.task('default', function(done){
 });
 
 gulp.task('html', function(done){
-    console.log("\n some useful task \n ");
+    browsrSync.reload();
     done();
 });
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssvars,nestvars,autoprefixer]))
-    .pipe(gulp.dest('.app/temp/styles')
- 
+    .pipe(postcss([cssImport,cssvars,nestvars,autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles')
     );
 });
 
-gulp.task('watch', function(){
-    watch('./app/*.html', gulp.series('html'));
-    watch('./app/assets/styles/**/*.css', gulp.series('styles'));
+gulp.task('cssInject', function(){
+    return gulp.src('./app/temp/styles/style.css')
+    .pipe(browsrSync.stream());
 
 
 });
+
+gulp.task('watch', function(){
+    browsrSync.init({
+        server: {
+            baseDir:"app"
+        }
+    });
+    watch('./app/*.html', gulp.series('html'));
+    watch('./app/assets/styles/**/*.css', gulp.series('styles','cssInject'));
+
+
+});
+
