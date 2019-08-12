a33aa66853aa5027872c756f4300adb44d4e7f7c
Mixael <Mixaeljcb@gmail.com>
2019-08-11 23:25:57 -0500

    Added new packages gulp to allow css variables and nested styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nestvars = require('postcss-nested');
 
 gulp.task('default', function(done){
     console.log("Congrats in the first gulp task ");
@@ -10,14 +14,17 @@ gulp.task('html', function(done){
     console.log("\n some useful task \n ");
     done();
 });
-gulp.task('styles', function(done){
-    console.log(" \n styles \n ")
-    done();
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssvars,nestvars,autoprefixer]))
+    .pipe(gulp.dest('.app/temp/styles')
+ 
+    );
 });
 
-gulp.task('watch', function(done){
+gulp.task('watch', function(){
     watch('./app/*.html', gulp.series('html'));
     watch('./app/assets/styles/**/*.css', gulp.series('styles'));
-    done();
+
 
 });
