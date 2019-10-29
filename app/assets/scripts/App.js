a09348798983f43a840db4a196da5969561fcb5d
Mixael Contreras <Mixaeljcb@gmail.com>
2019-10-29 06:50:45 -0500

    Completed reveal on scroll for featured_items class and Testimonials Class

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,11 @@
 
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 // No imports or requirements Below this line 
 
 let mobileMenu = new MobileMenu();
+//let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".featured-item"),"85%");
+new RevealOnScroll($(".testimonial"),"60%");
