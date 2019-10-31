de86432e2e5c526d1e587bae1c221690175f8439
Mixael Contreras <Mixaeljcb@gmail.com>
2019-10-31 08:08:52 -0500

    Finished modal pop-up on contact form

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,7 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 // No imports or requirements Below this line 
 
@@ -11,3 +12,4 @@ let mobileMenu = new MobileMenu();
 new RevealOnScroll($(".featured-item"),"85%");
 new RevealOnScroll($(".testimonial"),"60%");
 let stickyHeader = new StickyHeader();
+let modal = new Modal();
