2156ce4b1d8d6a69c19f9e533c2f0f84c1887354
Mixael Contreras <Mixaeljcb@gmail.com>
2019-11-02 00:18:24 -0500

    Added lazy loading of images feature

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 
 module.exports = {
-    entry: "./App/assets/scripts/App.js",
+    entry: {
+      App:"./App/assets/scripts/App.js",
+      Vendor: "./App/assets/scripts/Vendor.js"
+    },
     output: {
         path: path.resolve(__dirname,"./app/temp/scripts"),
-        filename: "App.js"
+        filename: "[name].js"
     }, 
     module: {
         rules: [
